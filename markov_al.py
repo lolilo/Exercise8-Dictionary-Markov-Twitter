75e869ee5b13e3a4f800ea8525fcabe150e1ce9c
Amy <amy.mae.lee@gmail.com>
2014-02-12 00:23:51 -0800

    Mashes 2 files

--- markov_al.py
+++ markov_al.py
@@ -3,6 +3,18 @@
 import sys
 import random
 
+#COMMENT: get raw input from user for n grams. (this was originally in make_chains() function)
+n = int(raw_input("How many n's in your grams? > "))
+
+#COMMENT: get raw input of length of Markov string requested (this was originally in make_text() function)
+x = int(raw_input("length of your sentence? > "))
+    # TO-DO QUESTION A: Can I set x and n above as global variables so can be referenced in both fucntions
+    #    or can/should I put in MAIN function?
+    #   (That way I can reference n by typing x-n and then range(x-n) below to get the actual correct # of 
+    #   words requested by user input) - See TODO Question B below
+
+
+
 def make_chains(corpus):
     """Takes an input text as a string and returns a dictionary of
     markov chains."""
@@ -10,9 +22,6 @@ def make_chains(corpus):
     #COMMENT: Take text_body and split into list of words by white space
     word_list = corpus.split()
     # print word_list
-
-    #COMMENT: get raw input from user for n grams
-    n = int(raw_input("How many n's in your grams? >"))
 
     #COMMENT: loop through to create dictionary key of n-gram strings: paired with next word
     markov_dict = {}
@@ -32,34 +41,37 @@ def make_text(chains):
     """Takes a dictionary of markov chains and returns random text
     based off an original text."""
 
-    x = int(raw_input("length of your sentence? >"))
-    # TO-DO QUESTION: If wanted to, could I set this and n above as global variables and then reference 
-    #   them from inside the function? That way I can reference n by typing x-n and then range(x-n) below
-    #   to get the actual correct # of words requested by user input
 
     markov_string = random.choice(chains.keys())
-    # print start_key
+
+    # COMMENT Take the start key and choose randomly from associated values
+    new_word = random.choice(chains[markov_string])
+    # print "this is your start string: ", markov_string
+    # print "this is the word to be added to the string: ", new_word
+
     for i in range(x):
-        new_word = random.choice(chains[markov_key])
-        markov_string += ' ' + random.choice(chains[markov_key])
+        # COMMENT append word to exxisting string
+        markov_string += ' ' + new_word
+        # COMMENT Take the existing string and split into list temporarily
+        markov_string_list = markov_string.split()
+        # COMMENT join the last n items as new look up key
+        new_key = " ".join(markov_string_list[-n:])
+        # COMMENT use new look up key to find associated values to choose randomly from
+        new_word = random.choice(chains[new_key])
 
-
-        # Take the start key and look up associated values to choose randomly from
-        # concatenate start key with random value
-        # Take the existing string and split into list temporarily, join the last n items as new look up key
-        # use new look up key to find associated values to choose randomly from
-
-        start_key = 
-
-    return "Here's some random text."
-    # return markov_string
+    return markov_string
+    # print "your markov_string is: ", markov_string
 
 def main():
-    script, firstfile  = sys.argv
+    script, firstfile, secondfile  = sys.argv
 
     #COMMENT: Change this to read input_text from a file
-    f = open("test.txt")
-    input_text = f.read()
+    f1 = open(firstfile)
+    f2 = open(secondfile)
+    input_text1 = f1.read()
+    input_text2 = f2.read()
+
+    input_text = input_text1 + " " + input_text2
     # print input_text
 
     chain_dict = make_chains(input_text)
@@ -71,10 +83,11 @@ if __name__ == "__main__":
 
 
 # TO-DO LIST
-# Put in check if input file exists to give feedback
-# How would you have an unlimited number of argv that you could input from command line? 
-
 # put in two files or more
+# Do the checks for capital letters and end puncutation
+# Put in check if input file exists and give feedback if not
+
+# Extra: How would you have an unlimited number of argv that you could input from command line? 
+
 # check against markov.py methodology
-# Create a new Twitter persona and 
-#   wire up your markov program with the twitter module (import twitter) to produce random tweets.
+# Create a new Twitter persona and wire up your markov program with the twitter module (import twitter) to produce random tweets.
